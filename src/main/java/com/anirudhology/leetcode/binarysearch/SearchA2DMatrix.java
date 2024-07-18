419eb88731aa69c334925cba53c6116f9eca7776
anirudhology <anirudhology@gmail.com>
2024-07-18 06:40:40 +0530

    Add problem 74 - Search a 2D Matrix

--- src/main/java/com/anirudhology/leetcode/binarysearch/SearchA2DMatrix.java
+++ src/main/java/com/anirudhology/leetcode/binarysearch/SearchA2DMatrix.java
@@ -7,29 +7,23 @@ public class SearchA2DMatrix {
         if (matrix == null || matrix.length == 0) {
             return false;
         }
-        int n = matrix[0].length;
-        // Process matrix row by row
-        for (int[] row : matrix) {
-            if (row[0] <= target && row[n - 1] >= target) {
-                return doesElementExist(row, n, target);
-            }
-        }
-        return false;
-    }
-
-    private boolean doesElementExist(int[] row, int n, int target) {
-        // Left and right pointers
-        int left = 0;
-        int right = n - 1;
-        // Process the row from both ends
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (row[middle] == target) {
+        // Dimensions of the matrix
+        final int m = matrix.length;
+        final int n = matrix[0].length;
+        // Start and end pointers
+        int start = 0;
+        int end = m * n - 1;
+        // Process the matrix
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            int i = middle / n;
+            int j = middle % n;
+            if (matrix[i][j] == target) {
                 return true;
-            } else if (row[middle] < target) {
-                left = middle + 1;
+            } else if (matrix[i][j] < target) {
+                start = middle + 1;
             } else {
-                right = middle - 1;
+                end = middle - 1;
             }
         }
         return false;

be896812b98c63aac97a86fd86f8b50a4ca8416b
anirudhology <anirudhology@gmail.com>
2024-08-06 05:14:16 +0530

    Add problem 703 - Kth Lrgest Element In A Stream

---
+++ src/main/java/com/anirudhology/leetcode/KthLargestElementInAStream.java
@@ -0,0 +1,35 @@
+package com.anirudhology.leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargestElementInAStream {
+
+    static class KthLargest {
+
+        // Queue to represent min heap
+        private final Queue<Integer> elements;
+        // Capacity
+        private final int capacity;
+
+        public KthLargest(int k, int[] nums) {
+            this.elements = new PriorityQueue<>();
+            this.capacity = k;
+            // Add k elements from nums to the min heap
+            for (int num : nums) {
+                add(num);
+            }
+        }
+
+        public int add(int val) {
+            // If we have fewer than k elements
+            if (this.elements.size() < capacity) {
+                this.elements.offer(val);
+            } else if (this.elements.peek() < val) {
+                this.elements.remove();
+                this.elements.offer(val);
+            }
+            return this.elements.peek();
+        }
+    }
+}
---
+++ src/test/java/com/anirudhology/leetcode/KthLargestElementInAStreamTest.java
@@ -0,0 +1,21 @@
+package com.anirudhology.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.anirudhology.leetcode.KthLargestElementInAStream.KthLargest;
+
+class KthLargestElementInAStreamTest {
+
+    @Test
+    public void testKthLargest() {
+        int[] nums = {4, 5, 8, 2};
+        KthLargest kthLargest = new KthLargest(3, nums);
+
+        assertEquals(4, kthLargest.add(3));   // returns 4
+        assertEquals(5, kthLargest.add(5));   // returns 5
+        assertEquals(5, kthLargest.add(10));  // returns 5
+        assertEquals(8, kthLargest.add(9));   // returns 8
+        assertEquals(8, kthLargest.add(4));   // returns 8
+    }
+}

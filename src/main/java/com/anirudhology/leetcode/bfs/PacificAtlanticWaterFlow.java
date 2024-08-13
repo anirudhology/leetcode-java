bce38b41646d2a427039d7d0d4d2a289ba05e741
anirudhology <anirudhology@gmail.com>
2024-08-13 06:20:28 +0530

    Update Problem 417 - Pacific Atlantic Water Flow

--- src/main/java/com/anirudhology/leetcode/bfs/PacificAtlanticWaterFlow.java
+++ src/main/java/com/anirudhology/leetcode/bfs/PacificAtlanticWaterFlow.java
@@ -8,47 +8,47 @@ import java.util.List;
 public class PacificAtlanticWaterFlow {
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-    // List to store the valid coordinates
-    final List<List<Integer>> coordinates = new ArrayList<>();
-    // Special case
-    if (heights == null || heights.length == 0) {
-        return coordinates;
-    }
-    // Dimensions of the island
-    final int m = heights.length;
-    final int n = heights[0].length;
-    // Boolean arrays to keep track of visited cells
-    final boolean[][] pacificVisited = new boolean[m][n];
-    final boolean[][] atlanticVisited = new boolean[m][n];
-    // Queues to perform BFS for both the oceans
-    final Deque<int[]> pacificCells = new ArrayDeque<>();
-    final Deque<int[]> atlanticCells = new ArrayDeque<>();
-    // Populate the coordinates of pacific and atlantic borders
-    for (int i = 0; i < m; i++) {
-        pacificCells.offer(new int[]{i, 0});
-        atlanticCells.offer(new int[]{i, n - 1});
-        pacificVisited[i][0] = true;
-        atlanticVisited[i][n - 1] = true;
-    }
-    for (int j = 0; j < n; j++) {
-        pacificCells.offer(new int[]{0, j});
-        atlanticCells.offer(new int[]{m - 1, j});
-        pacificVisited[0][j] = true;
-        atlanticVisited[m - 1][j] = true;
-    }
-    // Perform BFS on both the queues
-    bfs(heights, pacificCells, pacificVisited, m, n);
-    bfs(heights, atlanticCells, atlanticVisited, m, n);
-    // Combine visited cells from both oceans
-    for (int i = 0; i < m; i++) {
+        // List to store the valid coordinates
+        final List<List<Integer>> coordinates = new ArrayList<>();
+        // Special case
+        if (heights == null || heights.length == 0) {
+            return coordinates;
+        }
+        // Dimensions of the island
+        final int m = heights.length;
+        final int n = heights[0].length;
+        // Boolean arrays to keep track of visited cells
+        final boolean[][] pacificVisited = new boolean[m][n];
+        final boolean[][] atlanticVisited = new boolean[m][n];
+        // Queues to perform BFS for both the oceans
+        final Deque<int[]> pacificCells = new ArrayDeque<>();
+        final Deque<int[]> atlanticCells = new ArrayDeque<>();
+        // Populate the coordinates of pacific and atlantic borders
+        for (int i = 0; i < m; i++) {
+            pacificCells.offer(new int[]{i, 0});
+            atlanticCells.offer(new int[]{i, n - 1});
+            pacificVisited[i][0] = true;
+            atlanticVisited[i][n - 1] = true;
+        }
         for (int j = 0; j < n; j++) {
-            if (pacificVisited[i][j] && atlanticVisited[i][j]) {
-                coordinates.add(List.of(i, j));
+            pacificCells.offer(new int[]{0, j});
+            atlanticCells.offer(new int[]{m - 1, j});
+            pacificVisited[0][j] = true;
+            atlanticVisited[m - 1][j] = true;
+        }
+        // Perform BFS on both the queues
+        bfs(heights, pacificCells, pacificVisited, m, n);
+        bfs(heights, atlanticCells, atlanticVisited, m, n);
+        // Combine visited cells from both oceans
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacificVisited[i][j] && atlanticVisited[i][j]) {
+                    coordinates.add(List.of(i, j));
+                }
             }
         }
+        return coordinates;
     }
-    return coordinates;
-}
 
     private void bfs(int[][] heights, Deque<int[]> cells, boolean[][] visited, int m, int n) {
         // Four directions
